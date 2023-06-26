dd82a4893867f2458693aae1b97a45c807f06892
jgbarbosa2356 <137813412+jgbarbosa2356@users.noreply.github.com>
2023-06-26 17:14:07 -0300

    Atividade 26\06 - Tquarta-feira
    atividades de variáveis, mandei aqui e na plataforma mandei o print

---
+++ js - const, var, let.js
@@ -0,0 +1,14 @@
+var nome1 = "joão";
+console.log(nome1);
+var nome2 = "maria";
+console.log(nome2);
+
+let  num1 = 15
+console.log(num1);
+let  nome3 = "Julia"
+console.log(nome3);
+
+const pi = 3.14
+console.log(pi)
+const num4 = 55
+console.log(num4)
---
+++ js - variáveis.js
@@ -0,0 +1,43 @@
+var num1 = 10;
+console.log(num1);
+var num2 = 2;
+console.log(num2);
+
+var nome1 = "joão";
+console.log(nome1);
+var nome2 = "Kemysson";
+console.log(nome2);
+
+var verdade = true;
+var mentira = false;
+console.log("está na turma de front?");
+console.log(verdade);
+
+console.log("está na turma de back?");
+console.log(mentira);
+
+var verdade = true;
+var mentira = false;
+console.log("hoje é sexta");
+console.log(verdade);
+
+console.log("hoje é domingo");
+console.log(mentira);
+
+
+var frutas = ["mamão", "melancia", "maça"];
+console.log(frutas);
+
+var verduras = ["cebola", "cenoura", "abobora"]
+console.log(verduras);
+
+var inf = {nome: "joão", idade:18, altura:1.87}
+console.log(inf);
+
+var carro = {modelo: "Hrv", ano:2023, cor:"vinho"}
+console.log(carro);
+
+var vazio = null
+console.log("valor vazio: " + vazio);
+var valor_qualquer;
+console.log("valor qualquer: " + valor_qualquer);
